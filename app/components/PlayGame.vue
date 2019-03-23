b513e1e57d24814c2c279a0032fdbd50d46e0254
Abel Calluaud <abelcd@protonmail.com>
2019-03-23 16:25:14 +0100

    install vur router

---
+++ app/components/PlayGame.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+    <div class="play">
+        <div class="play-column">
+            <h1>column</h1>
+        </div>
+        <div class="play-column">
+            <h2>column</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("hello")
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.play {
+    width: 100vw;
+    margin-top: 53px;
+    height: calc(100vh - 53px);
+    display: flex;
+}
+</style>
